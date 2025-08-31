02e090787fb0b28022853a0de1a77c25fec84deb
Andres Andrade <aaandrades@outlook.com>
2025-08-31 08:21:57 -0400

    Changes from Chat Participant

--- src/components/TicketDetails/index.tsx
+++ src/components/TicketDetails/index.tsx
@@ -15,6 +15,10 @@ const TicketDetails = () => {
     submitDescriptionEvent(scrumDescription);
   };
 
+  const handleHelloClick = () => {
+    console.log("hello world");
+  };
+
   return (
     <article className="ticket-details">
       <h2 className="ticket-details__title">Ticket details</h2>
@@ -47,6 +51,11 @@ const TicketDetails = () => {
           <em>No description</em>
         </p>
       )}
+
+      {/* Hello World button for OPS-7 */}
+      <div style={{ marginTop: 16 }}>
+        <Button type="button" label="Hello world" onClick={handleHelloClick} />
+      </div>
     </article>
   );
 };
