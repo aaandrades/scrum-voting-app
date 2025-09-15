810d876f3663f254e4a07d43ca4d30c524ce0d60
Andres Andrade <aaandrades@outlook.com>
2025-09-15 15:58:21 -0400

    Changes from Chat Participant

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,50 +1,16 @@
-import { SyntheticEvent, useState } from "react";
-import "./styles.css";
-import Checkbox from "../Checkbox";
-import Button from "../Button";
-import { User } from "../../types/main";
-import Input from "../Input";
+import React from 'react';
+import TermsConditionsEntry from './TermsConditionsEntry';
+import './Login.css';
 
-interface LoginProps {
-  onActivated: (form: User) => void;
-}
-
-const Login = ({ onActivated }: LoginProps) => {
-  const [form, setForm] = useState<User>({ id: "", scrum: false, name: "" });
-
-  const handleChange = (e: any, multiText = false) => {
-    setForm({
-      ...form,
-      [e.target.name]: multiText ? e.target.value : e.target.checked,
-    });
-  };
-
-  const onSubmit = (e: SyntheticEvent) => {
-    e.preventDefault();
-    onActivated(form);
-  };
-
+const Login: React.FC = () => {
   return (
-    <form className="login" onSubmit={onSubmit}>
-      <h2 className="login__title">Scrum voting session</h2>
-      <p className="login__description">
-        Set up your planning poker in seconds, start estimating story points in
-        scrum poker now
-      </p>
-      <div className="login__form">
-        <Input
-          className="general-input"
-          id="login"
-          name="name"
-          type="text"
-          placeholder="Enter your name"
-          value={form.name}
-          onChange={(e: any) => handleChange(e, true)}
-        />
-        <Checkbox value={form.scrum} onChange={handleChange} />
-        <Button type="submit" label="Join room" disabled={!form.name} />
-      </div>
-    </form>
+    <div className="login">
+      {/* Existing login UI goes here */}
+      {/* ... */}
+
+      {/* Terms & Conditions trigger and modal */}
+      <TermsConditionsEntry />
+    </div>
   );
 };
 
